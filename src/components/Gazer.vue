a3e9d5c9013bc7d2232c0e1d1b4caf7049c5095c
EasonC13 <gtes9950406@gmail.com>
2022-01-16 07:54:46 +0800

    get facemesh points

--- src/components/Gazer.vue
+++ src/components/Gazer.vue
@@ -2,20 +2,53 @@
   <div>
       <button @click="getCurrent">Get Point</button>
       <button @click="hideGazerVideoContainer">hideGazerVideoContainer</button>
+      <button @click="clearGazer">ClearModel</button>
+      <p>{{current_emotion}}</p>
   </div>
 </template>
 <script>
-
+const axios = require('axios');
 export default {
+  data (){
+    return {
+      current_emotion: "None",
+    }
+  },
   methods: {
     async getCurrent(){
-      console.log("getPositions", webgazer.getTracker().getPositions())
-      var prediction = await webgazer.getCurrentPrediction();
+      // console.log("getPositions", webgazer.getTracker().getPositions())
+      let prediction = await webgazer.getCurrentPrediction();
       if (prediction) {
           var x = prediction.x;
           var y = prediction.y;
       }
-      console.log(prediction)
+      let video = document.getElementById("webgazerVideoFeed")
+      const canvas = document.createElement("canvas");
+      // scale the canvas accordingly
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      // draw the video at that frame
+      canvas.getContext('2d')
+        .drawImage(video, 0, 0, canvas.width, canvas.height);
+      // convert it to a usable data URL
+      let b64img = canvas.toDataURL();
+      // console.log(prediction)
+      prediction = await webgazer.get_facemash_model_predict()
+      let vue = this
+      axios({
+        method: "POST",
+        url: `https://gsoc.demo.eason.tw/model/facemesh`, 
+        headers: {
+                "accept": "application/json",
+                'Content-Type': 'application/json'
+        },
+        data: {
+            prediction: prediction,
+        },
+      }).then(response => {
+        console.log(response.data)
+        vue.current_emotion = response.data.emotion
+      })
                       
     },
     hideGazerVideoContainer(){
@@ -31,8 +64,12 @@ export default {
       document.getElementById("webgazerGazeDot").style.display = 'block'
     },
   },
-  mounted(){
-    webgazer.begin()
+  async mounted(){
+    await webgazer.begin()
+    // await webgazer.pause()
+  },
+  async clearGazer(){
+    await webgazer.clearData()
   }
 }
 </script>
