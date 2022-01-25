4f6e24be1f6e9f76d94c154266d224883d31e831
EasonC13 <gtes9950406@gmail.com>
2022-01-25 17:54:20 +0800

    remove footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,21 @@
 <template lang="">
+  <div>
     <footer class="footer mt-5 text-muted">
       <div class="container">
-        <span class="">本網站由「Focus 福課使」團隊製作，歡迎分享</span>
+        <!-- <span class="">本網站由「Focus 福課使」團隊製作，歡迎分享</span> -->
       </div>
     </footer>
+  </div>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
+<style lang="css">
+    .mainFooter{
+      position: relative;
+      bottom: 0;
+      width: 100%;
+    }
 </style>
